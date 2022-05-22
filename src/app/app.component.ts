02188112718e0d17fe7283685eb1aa56730ea1b0
Lav Pandey <lavpandey63@gmail.com>
2022-05-22 21:10:31 +0530

    Navigation buttons added, scroll to top button added, improved logic for courses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dayawati';
+
+  isViewMobile: boolean = false;
+
+  ngOnInit(): void {
+    this.isViewMobile = this.detectMobile();
+  }
+
+  navMenuItemClicked(menuItem: string) {
+    if (menuItem == 'contact_us') {
+      window.scrollTo(0, document.body.scrollHeight);
+    }
+    if (menuItem == 'courses') {
+      if (!this.isViewMobile) {
+        scroll(0, 700);
+      } else {
+        scroll(0, 900);
+      }
+    }
+  }
+
+  detectMobile() {
+    const isMobile = window.matchMedia(
+      'only screen and (max-width: 912px)'
+    ).matches;
+    return isMobile;
+  }
 }
