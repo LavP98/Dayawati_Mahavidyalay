3670b50a8d5f23bff196fadc4b9d26c07c7759b7
Lav Pandey <lavpandey63@gmail.com>
2022-05-22 19:48:16 +0530

    Added expansion panel for courses

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -6,7 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.css'],
 })
 export class CoursesComponent implements OnInit {
-  constructor() {}
+  hideBa: boolean = true;
+  hideBsc: boolean = true;
+  hideBscAgriculture: boolean = true;
+  courseDetails: any;
+  showExpansion: boolean = false;
+  constructor(private coursesService: CoursesService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.courseDetails = this.coursesService.courseDetails;
+  }
+
+  toggleExpansionSubjectsPanel(subject: string) {
+    this.hideBa = true;
+    this.hideBsc = true;
+    this.hideBscAgriculture = true;
+
+    switch (subject) {
+      case 'BA':
+        this.hideBa = false;
+        break;
+      case 'BSc':
+        this.hideBsc = false;
+        break;
+      case 'BSc Agriculture':
+        this.hideBscAgriculture = false;
+        break;
+    }
+  }
+
+  getCourseDetails(subject: string) {
+    let course = this.courseDetails.find(
+      (obj: { courseName: string }) => obj.courseName == subject
+    );
+    if (!course) {
+      return;
+    }
+    return course;
+  }
 }
